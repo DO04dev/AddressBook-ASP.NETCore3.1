30cbf83980b2cf0b8b165356bf3fc40ac1981859
Thabo Roy <thaboroy@gmail.com>
2020-10-19 01:44:49 +0200

    Web API Test

--- AddressBook.API/Controllers/ContactController.cs
+++ AddressBook.API/Controllers/ContactController.cs
@@ -54,7 +54,7 @@ namespace AddressBook.Controllers
                     .Set<ContactDetail>()
                     .SingleOrDefaultAsync(x => x.ContactDetailId == id);
 
-                if (data == null) return StatusCode((int)HttpStatusCode.NoContent);
+                if (data == null) return StatusCode((int)HttpStatusCode.NotFound, $"Could Not Found the record Of Id {id.ToString()}");
 
                 return Ok(data);
             }
@@ -101,9 +101,26 @@ namespace AddressBook.Controllers
                     return BadRequest(ModelState);
                 }
 
-                var data = await this._context.Set<ContactDetail>().FindAsync(id);
+                var data = await this._context.Set<ContactDetail>().SingleOrDefaultAsync(x => x.ContactDetailId == id);
 
-                data = value;
+
+                //this._context.Entry(data).State = EntityState.Detached;
+
+
+                data.ContactDetailId = value.ContactDetailId;
+                data.Description = value.Description;
+                data.Contact.ContactId = value.Contact.ContactId;
+                data.Contact.FirstName = value.Contact.FirstName;
+                data.Contact.Surname = value.Contact.Surname;
+                data.Contact.UpdatedDate = value.Contact.UpdatedDate;
+                data.Contact.BirthDate = value.Contact.BirthDate;
+                data.ContactType.ContactTypeId = value.ContactType.ContactTypeId;
+                data.ContactType.Address = value.ContactType.Address;
+                data.ContactType.Telephone = value.ContactType.Telephone;
+                data.ContactType.Cell = value.ContactType.Cell;
+                data.ContactType.Email = value.ContactType.Email;
+
+                //this._context.Attach<ContactDetail>(data);
 
                 this._context
                     .Set<ContactDetail>()
@@ -122,8 +139,27 @@ namespace AddressBook.Controllers
 
         // DELETE api/<ContactController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<ActionResult> Delete(long id)
         {
+            try
+            {
+                var data = await this._context
+                .Set<ContactDetail>()
+                .SingleOrDefaultAsync(x => x.ContactDetailId == id);
+
+                if (data == null) return StatusCode((int)HttpStatusCode.NotFound, "Colud not find this record to delete");
+
+                this._context.ContactDetails.Remove(data);
+
+                var success = await this._context.SaveChangesAsync() > 0;
+
+
+                return success ? Ok("Record deleted Successfully") : StatusCode((int)HttpStatusCode.InternalServerError, "somethismg Happend while trying to delete data");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, "Something happened with our server please try again later");
+            }
         }
     }
 }
